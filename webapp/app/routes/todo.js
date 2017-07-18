dcfaf1437c9640338918aeebf260b96f229089fb
Alan David Nungaray Becerra <alan.nungaray@softtekclients.com>
2017-07-18 16:46:29 -0700

    harcoded model

--- webapp/app/routes/todo.js
+++ webapp/app/routes/todo.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+      model(){
+        return {
+            todos:[{id:1,task:"asdf"}],
+            newTodo:{id:2,task:""}
+        }
+    }
 });
